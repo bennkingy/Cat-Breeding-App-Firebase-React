3ae8a8eefea9ace1c266ca593ae10ad99b744cdd
Benn <benn.king@intelogy.co.uk>
2020-07-12 17:35:52 +0100

    tailwind is installed properly!

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,4 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.config.js'),
-        require('autoprefixer'),
-    ],
+  plugins: [require("tailwindcss"), require("autoprefixer")]
 };
