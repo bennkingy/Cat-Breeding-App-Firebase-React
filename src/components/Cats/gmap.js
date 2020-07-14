131b75648521845dbe721d5a932b21d5ce1b73dc
Benn <benn.king@intelogy.co.uk>
2020-07-14 14:35:35 +0100

    Cat image now appears on marker pop up

--- src/components/Cats/gmap.js
+++ src/components/Cats/gmap.js
@@ -33,7 +33,7 @@ export class MapContainer extends Component {
 
     this.props.cats.map((cat) => {
       var popup = new mapboxgl.Popup({ offset: 25 })
-      .setHTML('<img src="' + cat.image + "'/>" + '<p>' + cat.text + '</p>')
+      .setHTML('<img src="' + cat.imageURL + '"/>' + '<h2>' + cat.text + '</h2>')
       let marker = new mapboxgl.Marker()
       .setLngLat([cat.lng, cat.lat])
       .setPopup(popup) 
