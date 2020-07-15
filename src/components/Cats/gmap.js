80c36d6bd5bfc6f046d8d567aa0cf3f1fe6bc365
Benn <benn.king@intelogy.co.uk>
2020-07-15 11:34:59 +0100

    Made good progress on the mapbox react library

--- src/components/Cats/gmap.js
+++ src/components/Cats/gmap.js
@@ -1,56 +1,40 @@
-import React, { Component, Fragment } from 'react';
-import mapboxgl from 'mapbox-gl';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmVubmtpbmd5IiwiYSI6ImNrY2ozMnJ5dzBrZ28ycnA1b2Vqb2I0bXgifQ.ZOaVtzsDQOrAovh9Orh13Q'
+import React, { useState, useEffect, Component } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl"; 
 
 export class MapContainer extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 2
+      viewport: {
+        latitude: 51.5074,
+        longitude: 0.1278,
+        width: "100vw",
+        height: "100vh",
+        zoom: 2
+      }
     };
   }
 
-  componentWillReceiveProps() {
-
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lat, this.state.lng],
-      zoom: this.state.zoom
-    });
-
-    map.on('move', () => {
-      this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
-    });
-
-    this.props.cats.map((cat) => {
-      var popup = new mapboxgl.Popup({ offset: 25 })
-      .setHTML('<div style="background:url(' + cat.imageURL + ');width:200px;height:200px;background-size:cover;background-position: center center;"></div>' + '<h2>' + cat.text + '</h2>')
-      let marker = new mapboxgl.Marker()
-      .setLngLat([cat.lng, cat.lat])
-      .setPopup(popup) 
-      .addTo(map);
-      // create the marke
-    });
-
-    //map.setView(markersLayer.getBounds().getCenter());
-
-  }
-
   render() {
-
     return (
-      <Fragment>
-        <div ref={el => this.mapContainer = el}  className="mapContainer" />
-      </Fragment>
+      <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOXPKEY}
+        //mapStyle="mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4"
+      >
+        { this.props.cats.map(cat => (
+          <Marker
+            key={cat.uid}
+            latitude={cat.lat}
+            longitude={cat.lng}
+          >
+            <div>{cat.text}</div>
+          </Marker>
+         ))
+       }
+      </ReactMapGL>
     );
   }
 }
